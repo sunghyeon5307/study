3ce44f31939357a4c93355369bc022c50824e929
sunghyeon5307 <sunghyeon5307@gmail.com>
2025-08-12 11:28:00 +0900

    bytetrack논문정리

--- 8:5~810 study/08:07_ObjectTracking_VideoInference/tracking.py
+++ 8:5~810 study/08:07_ObjectTracking_VideoInference/tracking.py
@@ -1,9 +1,9 @@
 from ultralytics import YOLO
 
-model = YOLO("yolov8n.pt")  # 또는 best.pt
+model = YOLO("yolov8n.pt")
 model.track(
-    source="videos/drive.mp4",  # 영상 경로
-    tracker="bytetrack.yaml",   # 기본값이라 생략 가능
-    show=True,                  # 영상 창 표시
-    persist=True                # 같은 영상 내 ID 유지
+    source="videos/drive.mp4",  
+    tracker="bytetrack.yaml",   
+    show=True,              
+    persist=True                
 )
